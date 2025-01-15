14a56378adcc8b1cfe9292d8c1867839ced461c9
NICOLA NSENGIYUMVA <fatepepe66@gmail.com>
2025-01-15 16:15:54 +0200

    eigth commit

--- src/components/TaskArea.tsx
+++ src/components/TaskArea.tsx
@@ -37,8 +37,8 @@ const TaskArea: React.FC = () => {
       <div className=" w-full p-5 mb-6">
         <h1 className=" font-bold text-gray-300">To Do</h1>
 
-        <div className={`w-full ${theme === 'light' ? 'bg-[#ffffff]' : 'bg-[#1B1B1B]'} py-3  pt-6 gap-24 space-y-8 flex-col`}>
-          <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} placeholder="todo..."   className={`w-full h-4 p-5 ${theme === 'light' ? 'bg-[#ffffff]' : 'bg-[#1B1B1B]'} `}/>
+        <div className={`w-full ${theme === 'light' ? 'bg-[#EEF6EF]' : 'bg-[#1B1B1B]'} py-3  pt-6 gap-24 space-y-8 flex-col`}>
+          <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)} placeholder="todo..."   className={`w-full h-4 p-5 ${theme === 'light' ? 'bg-[#EEF6EF]' : 'bg-[#1B1B1B]'} `}/>
           <div className="flex items-center justify-between gap-4">
           <div>
           <button className="p-2 ml-2 rounded-full text-gray-500 hover:bg-gray-100">
